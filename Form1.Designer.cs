d89d3b12ba0e56bd042b0edafc2c961dba4d2879
YoonGwngHyuck <YGH@YGH-Laptop>
2023-09-13 11:44:14 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.conn_btn = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.led_off_btn = new System.Windows.Forms.Button();
+            this.led_on_btn = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.led_text = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.tmp_text = new System.Windows.Forms.TextBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.light_text = new System.Windows.Forms.TextBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.dis_text = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.conn_btn);
+            this.groupBox1.Location = new System.Drawing.Point(30, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(296, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "연결";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8",
+            "COM9",
+            "COM10",
+            "COM11",
+            "COM12",
+            "COM13",
+            "COM14"});
+            this.comboBox1.Location = new System.Drawing.Point(16, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 26);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // conn_btn
+            // 
+            this.conn_btn.Location = new System.Drawing.Point(164, 25);
+            this.conn_btn.Name = "conn_btn";
+            this.conn_btn.Size = new System.Drawing.Size(110, 28);
+            this.conn_btn.TabIndex = 1;
+            this.conn_btn.Text = "연결";
+            this.conn_btn.UseVisualStyleBackColor = true;
+            this.conn_btn.Click += new System.EventHandler(this.conn_btn_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.led_off_btn);
+            this.groupBox2.Controls.Add(this.led_on_btn);
+            this.groupBox2.Location = new System.Drawing.Point(30, 143);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(296, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "LED ON/OFF";
+            // 
+            // led_off_btn
+            // 
+            this.led_off_btn.Location = new System.Drawing.Point(159, 27);
+            this.led_off_btn.Name = "led_off_btn";
+            this.led_off_btn.Size = new System.Drawing.Size(131, 67);
+            this.led_off_btn.TabIndex = 1;
+            this.led_off_btn.Text = "OFF";
+            this.led_off_btn.UseVisualStyleBackColor = true;
+            this.led_off_btn.Click += new System.EventHandler(this.led_off_btn_Click);
+            // 
+            // led_on_btn
+            // 
+            this.led_on_btn.Location = new System.Drawing.Point(6, 27);
+            this.led_on_btn.Name = "led_on_btn";
+            this.led_on_btn.Size = new System.Drawing.Size(131, 67);
+            this.led_on_btn.TabIndex = 0;
+            this.led_on_btn.Text = "ON";
+            this.led_on_btn.UseVisualStyleBackColor = true;
+            this.led_on_btn.Click += new System.EventHandler(this.led_on_btn_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.led_text);
+            this.groupBox3.Location = new System.Drawing.Point(30, 279);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(296, 74);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "LED 상태";
+            // 
+            // led_text
+            // 
+            this.led_text.Location = new System.Drawing.Point(16, 27);
+            this.led_text.Name = "led_text";
+            this.led_text.ReadOnly = true;
+            this.led_text.Size = new System.Drawing.Size(258, 28);
+            this.led_text.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.tmp_text);
+            this.groupBox4.Location = new System.Drawing.Point(36, 371);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(296, 74);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "온도 센서";
+            // 
+            // tmp_text
+            // 
+            this.tmp_text.Location = new System.Drawing.Point(10, 27);
+            this.tmp_text.Name = "tmp_text";
+            this.tmp_text.ReadOnly = true;
+            this.tmp_text.Size = new System.Drawing.Size(258, 28);
+            this.tmp_text.TabIndex = 1;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.light_text);
+            this.groupBox5.Location = new System.Drawing.Point(30, 460);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(296, 74);
+            this.groupBox5.TabIndex = 3;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "조도 센서";
+            // 
+            // light_text
+            // 
+            this.light_text.Location = new System.Drawing.Point(16, 27);
+            this.light_text.Name = "light_text";
+            this.light_text.ReadOnly = true;
+            this.light_text.Size = new System.Drawing.Size(258, 28);
+            this.light_text.TabIndex = 2;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.dis_text);
+            this.groupBox6.Location = new System.Drawing.Point(30, 552);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(296, 74);
+            this.groupBox6.TabIndex = 4;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "초음파 센서";
+            // 
+            // dis_text
+            // 
+            this.dis_text.Location = new System.Drawing.Point(16, 27);
+            this.dis_text.Name = "dis_text";
+            this.dis_text.ReadOnly = true;
+            this.dis_text.Size = new System.Drawing.Size(258, 28);
+            this.dis_text.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 700);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "ArduinoUi";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button conn_btn;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button led_off_btn;
+        private System.Windows.Forms.Button led_on_btn;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox led_text;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox tmp_text;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.TextBox light_text;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.TextBox dis_text;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//추가
+using System.Net.Http;
+using System.Net;
+using System.Threading;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //-----------------------------------------------------------
+        //백그라운드 메세지 수신 스레드 참조 변수
+        //----------------------------------------------------------
+        private BackgroundWorker recvLedWorker;
+        private BackgroundWorker recvTmpWorker;
+        private BackgroundWorker recvLightWorker;
+        private BackgroundWorker recvdisWorker;
+
+
+        private void conn_btn_Click(object sender, EventArgs e)
+        {
+            String port = this.comboBox1.Items[this.comboBox1.SelectedIndex].ToString();
+            Console.WriteLine("PORT : " + port);
+            HttpWebRequest request = null;
+            HttpWebResponse response = null;
+
+            try { 
+            request = (HttpWebRequest)HttpWebRequest.Create("http://localhost:8080/arduino/conn/" + port);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            //request.Timeout = 30*1000;
+
+            response = (HttpWebResponse)request.GetResponse();
+            
+            if(response.StatusCode == HttpStatusCode.OK)
+            {
+                Console.WriteLine("RESPONSE CODE : " + response.StatusCode);
+                    //-------------------------------------------------------
+                    //수신 스레드 객체 생성 & 스레드 등록
+                    //--------------------------------------------------------
+                    //LED
+                    recvLedWorker = new BackgroundWorker();
+                    recvLedWorker.DoWork += recvLedInfo;
+
+                    //TMP
+                    recvTmpWorker = new BackgroundWorker();
+                    recvTmpWorker.DoWork += recvTmpInfo;
+
+                    //LIGTH
+                    recvLightWorker = new BackgroundWorker();
+                    recvLightWorker.DoWork += recvLightInfo;
+
+                    //DIS
+                    recvdisWorker = new BackgroundWorker();
+                    recvdisWorker.DoWork += recvdisInfo;
+
+
+                    //수신 쓰레드 실행
+                    recvLedWorker.RunWorkerAsync();
+                    recvTmpWorker.RunWorkerAsync();
+                    recvLightWorker.RunWorkerAsync();
+                    recvdisWorker.RunWorkerAsync();
+
+                }
+            }catch(Exception ex)
+            {
+                Console.WriteLine("EX : " + ex);
+            }
+        }
+
+        private void recvLedInfo(object sender,DoWorkEventArgs e)
+        {
+            while (!recvLedWorker.CancellationPending)
+            {
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://localhost:8080/arduino/ui/led");
+                request.Method = "GET";
+                request.ContentType = "application/json";
+                //request.Timeout = 30*1000;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader sr = new StreamReader(stream);
+
+                Invoke(new Action(() =>
+                {
+                    this.led_text.Text = sr.ReadToEnd();
+                }));
+
+
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void recvTmpInfo(object sender, DoWorkEventArgs e)
+        {
+            while (!recvTmpWorker.CancellationPending)
+            {
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://localhost:8080/arduino/ui/tmp");
+                request.Method = "GET";
+                request.ContentType = "application/json";
+                //request.Timeout = 30*1000;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader sr = new StreamReader(stream);
+
+                Invoke(new Action(() =>
+                {
+                    this.tmp_text.Text = sr.ReadToEnd();
+                }));
+
+
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void recvLightInfo(object sender, DoWorkEventArgs e)
+        {
+            while (!recvLightWorker.CancellationPending)
+            {
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://localhost:8080/arduino/ui/light");
+                request.Method = "GET";
+                request.ContentType = "application/json";
+                //request.Timeout = 30*1000;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader sr = new StreamReader(stream);
+
+                Invoke(new Action(() =>
+                {
+                    this.light_text.Text = sr.ReadToEnd();
+                }));
+
+
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void recvdisInfo(object sender, DoWorkEventArgs e)
+        {
+            while (!recvdisWorker.CancellationPending)
+            {
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://localhost:8080/arduino/ui/dis");
+                request.Method = "GET";
+                request.ContentType = "application/json";
+                //request.Timeout = 30*1000;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader sr = new StreamReader(stream);
+
+                Invoke(new Action(() =>
+                {
+                    this.dis_text.Text = sr.ReadToEnd();
+                }));
+
+
+
+                Thread.Sleep(1000);
+            }
+        }
+
+
+        private void led_on_btn_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://localhost:8080/arduino/led/1");
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            //request.Timeout = 30*1000;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        }
+
+        private void led_off_btn_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://localhost:8080/arduino/led/0");
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            //request.Timeout = 30*1000;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        }
+    }
+}
